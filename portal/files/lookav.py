06aa41a76c1668d8b80173103bb0d2c9db1c4ca5
PriyadharshanSaba <pyadhe@Priyadharshans-MacBook-Pro.local>
2017-11-22 19:08:36 +0530

    Python Files

---
+++ portal/files/lookav.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#from utils.utils import YourClassOrFunction
+
+from __future__ import unicode_literals
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+import mysql.connector
+from django.template import RequestContext
+import re
+import mechanize
+from pyscri import check_admin_login,putMarksCustomSem
+import csv
+
+
+def adminiHome(request):
+    return render(request,'admini/headtest_admini.html')
+
+def product(request):
+    return render(request,'admini/pro.html')
+
+
+def forgotLoginDetails(request):
+    return render(request,'admini/ad.html')
+
+
+def loginHome(request):
+    username = request.session['cur_admin']
+    admin_user = check_admin_login.fetchAdminName(username)
+    return render(request,'admini/home_ad.html',{'admin':[admin_user]})
+
+
+def marksDash(request):
+    return render(request,'admini/enterMarks.html')
+
+def okay(request):
+    startUsn = request.POST['startUSN']
+    endUsn = request.POST['endUSN']
+    sem = request.session['semester']
+    putMarksCustomSem.main_func(startUsn,endUsn,sem)
+    return render(request,'admini/dbokay.html')
+
+def testing(request):
+    sem=request.POST['sem']
+    request.session['semester']=sem
+    return render(request, 'admini/temptest.html' ,{'testing':[sem]})
+
+
+def login_redirection(request):
+    admin_name=request.POST['adminNAME']
+    admin_pass=request.POST['adminPSW']
+    request.session['cur_admin']= admin_name
+    checkDetails = check_admin_login.checkLoginInfo(admin_name.lower(),admin_pass)
+    if checkDetails == 1:
+        return render(request,'admini/login_redirection.html')
+    else:
+        return render(request,'admini/headtest_admini_incorrect.html')
+
+
+
