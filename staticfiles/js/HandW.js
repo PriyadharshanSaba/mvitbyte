fb0fb8eec02d66955152ca845b9dda6ae1926342
PriyadharshanSaba <priyadharshan.97@gmail.com>
2017-12-09 21:33:58 +0530

    login image fix

---
+++ staticfiles/js/HandW.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function () {
+                  var logTMargeSet = ($(window).height())/2.6;
+                  var logWset= ($(window).width())/2.25;
+                  $('.logg').css("margin-top",logTMargeSet+"px")
+                  $('.logg').css("margin-left",logWset+"px")
+                  });
+
+$(document).ready(function () {
+                  var winHeight = ($(window).height())
+                  var winWid= ($(window).width())
+                  $('.bbimg').css("width", winWid+"px");
+                  $('.bbimg').css("height",winHeight + "px");
+                  $('.model').css("width","50 px");
+                  alert(winWid);
+                  });
+
+
